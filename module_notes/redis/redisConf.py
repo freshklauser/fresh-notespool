899a77a966a2cb268cca0eccef06bad07f24ccc4
KlausLyu <junsnikylv@foxmail.com>
2019-11-27 17:49:45 +0800

    redis

--- module_notes/redis/redisConf.py
+++ module_notes/redis/redisConf.py
@@ -2,7 +2,7 @@
 # @Author: KlausLyu
 # @Date:   2019-11-21 18:59:40
 # @Last Modified by:   KlausLyu
-# @Last Modified time: 2019-11-21 20:19:32
+# @Last Modified time: 2019-11-22 09:57:32
 
 '''
 redis参数
@@ -13,13 +13,6 @@ REDIS_PORT = 6379
 REDIS_DB_TEST = 15
 REDIS_PASSWORD = 'spindle123456'
 
-REDIS_CHANNEL_LIST = []
-REDIS_CHANNEL_0 = 'flag'        # channel-0: 监听是否收到停止发布的信息,flag的内容为over则stop
-REDIS_CHANNEL_LIST.append(REDIS_CHANNEL_0)
-REDIS_CHANNEL_1 = 'spindle_collection'
-REDIS_CHANNEL_LIST.append(REDIS_CHANNEL_1)
-REDIS_CHANNEL_2 = 'football'
-REDIS_CHANNEL_LIST.append(REDIS_CHANNEL_2)
-REDIS_CHANNEL_3 = 'F1'
-REDIS_CHANNEL_LIST.append(REDIS_CHANNEL_3)
+REDIS_CHANNELS = ['192.168.1.1', '192.168.1.2', '192.168.1.3', '192.168.1.5']
+REDIS_TOPICS = ['group1', 'exitflag']
 
